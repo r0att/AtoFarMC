cc17484819a6a0fd4e9ca131d757a8ac6790e99d
r0att <jacob.budzyn@gmail.com>
2026-09-01 21:37:09 +0200

    Minor bugfix

--- src/models/farm.py
+++ src/models/farm.py
@@ -18,7 +18,7 @@ class Farm:
     created_at: datetime
 
     version: str = field(repr=False)
-    coordinates: tuple[int] | None = field(repr=False)
+    coordinates: tuple[int, int, int] | None = field(repr=False)
     description: str | None = field(repr=False)
     productivity: dict[str, float] = field(repr=False)
     access_password_hash: str = field(repr=False)
