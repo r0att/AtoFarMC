50d0462c5682e9c46ef795c3a7e5016c33f17e3c
r0att <jacob.budzyn@gmail.com>
2026-09-03 20:45:50 +0200

    Updated src/database/table_models/__init__.py

--- src/database/table_models/__init__.py
+++ src/database/table_models/__init__.py
@@ -3,3 +3,5 @@ from .user import UserTable
 from .world import WorldTable
 from .farm import FarmTable
 from .world_players import WorldPlayersTable
+from .user_favourite_farms import UserFavouriteFarms
+from .user_favourite_worlds import UserFavouriteWorlds
